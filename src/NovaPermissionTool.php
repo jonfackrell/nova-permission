4c96e8e1327373b978cd1cb5a58e1fe30ca819b7
Jefferson Simão Gonçalves <gerson.simao.92@gmail.com>
2022-04-03 14:56:41 -0300

    Corrigindo o import

--- src/NovaPermissionTool.php
+++ src/NovaPermissionTool.php
@@ -7,7 +7,7 @@ use JeffersonSimaoGoncalves\NovaPermission\Policy\PermissionPolicy;
 use JeffersonSimaoGoncalves\NovaPermission\Policy\RolePolicy;
 use Laravel\Nova\Nova;
 use Laravel\Nova\Tool;
-use function JeffersonSimaoGoncalves\NovaPermission\config;
+use function config;
 
 class NovaPermissionTool extends Tool
 {
